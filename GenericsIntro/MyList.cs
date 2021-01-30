246a8a6388d9574a004bcadc5699d8c0b9c3787d
Büşra <busrakc.10@gmail.com>
2021-01-30 16:27:32 +0200

    20 Ocak

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor = bir class new'lendiğinde çalıştığı blok
+        //array new'lenmek zorunda
+        public MyList()
+        {
+            items = new T[0];
+         }
+        public void Add(T item)
+        {
+            T[] tempArray = items;  // geçici dizinin referansı, items'ın referansı
+            items = new T[items.Length + 1];  //eleman sayısı 1 arttırılır 
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+    }
+}
---
+++ SQLQuery6.sql
@@ -0,0 +1,35 @@
+﻿--Select
+--ANSII
+Select ContactName Adi, CompanyName, City Sehir from Customers
+
+Select * from Customers where City = 'Berlin'
+
+--case insensitive
+select * from Products where categoryId=1 or categoryId=3
+
+select * from Products where categoryId=1 and UnitPrice>=10
+
+select * from Products where CategoryID=1 order by UnitPrice desc   --ascending --descending
+
+select count(*) Adet from Products --ürünlerin sayısı
+
+select categoryId,count(*) from products group by CategoryID having count(*)<10 --ürün sayısı 10 taneden az olan kateggoriler
+
+select categoryId,count(*) from products where UnitPrice>20 group by CategoryID having count(*)<10
+
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName
+from Products inner join Categories
+on Products.CategoryID = Categories.CategoryID
+where Products.UnitPrice>10
+
+--DTO Data Transformation Object
+
+select * from Products p inner join [Order Details] od
+on p.ProductID = od.ProductID
+inner join Orders o
+on o.OrderID = od.OrderID --bir join daha eklemek istersek inner join ekleriz
+
+select * from Customers c left join Orders o
+on c.CustomerID = o.CustomerID
+where o.CustomerID is null --hiç ürün almayan müşteriler
+
